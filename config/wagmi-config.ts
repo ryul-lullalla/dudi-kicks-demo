41ad93bbd71982aef5e4fb8ec1d56d2404237b9c
_ryul_lulalla <55829866+ryul-lullalla@users.noreply.github.com>
2025-07-14 12:17:11 +0900

    set wc config

--- config/wagmi-config.ts
+++ config/wagmi-config.ts
@@ -2,12 +2,23 @@ import { http, createConfig } from "wagmi";
 import { base, baseSepolia } from "wagmi/chains";
 import { coinbaseWallet, walletConnect } from "wagmi/connectors";
 
+const metadata = {
+  name: "Dudi Kicks",
+  description: "Score or Block. Just choose and shoot. Follow your heart.",
+  url: "https://dudi-kicks-demo.vercel.app",
+  icons: ["/icon.png"],
+};
+
 export const wagmiConfig = createConfig({
   chains: [base, baseSepolia], // prod + test
   multiInjectedProviderDiscovery: false,
   connectors: [
     coinbaseWallet({ appName: "My MiniApp" }),
-    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_ID! }),
+    walletConnect({
+      projectId: process.env.NEXT_PUBLIC_WC_ID!,
+      metadata,
+      showQrModal: false,
+    }),
   ],
   transports: { [base.id]: http(), [baseSepolia.id]: http() },
 });
