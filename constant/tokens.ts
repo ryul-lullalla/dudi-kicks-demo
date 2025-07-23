a1b9a2717bf2ac6fe98049eca5fafcf41ef42dad
_ryul_lulalla <55829866+ryul-lullalla@users.noreply.github.com>
2025-07-23 18:57:47 +0900

    modified contract and token env

--- constant/tokens.ts
+++ constant/tokens.ts
@@ -9,9 +9,9 @@ export const ETH_TOKEN_ADDRESS = "0x4200000000000000000000000000000000000001";
 // export const ETH_TOKEN_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export const KLEVA_TOKEN_ADDRESS: Record<number, `0x${string}`> = {
-  [baseMainnet.id]: "0xCE84616D287925ebE600b500BAd6d2A4b3EF563e",
+  [baseMainnet.id]: "0xea72500C0e6Ef6aca1A618aE7fCa44a28C6315Cf",
   // [baseTestnet.id]: "0xbFC99fbDD8F18b58796ee8398106325721721015",
-  [baseTestnet.id]: "0xCE84616D287925ebE600b500BAd6d2A4b3EF563e",
+  [baseTestnet.id]: "0xea72500C0e6Ef6aca1A618aE7fCa44a28C6315Cf",
 };
 
 export const USDC_TOKEN_ADDRESS: Record<number, `0x${string}`> = {
